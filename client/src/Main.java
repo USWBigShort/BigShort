fb3a605d373e0fc5ee8b663b9ee1876498a14996
koownij <koownij@kakao.com>
2022-12-10 12:58:13 +0900

    test commit

--- client/src/Main.java
+++ client/src/Main.java
@@ -3,6 +3,7 @@ import java.net.*;
 
 public class Main {
     public static void main(String[] args) {
+        //TEST
 
         Thread receiveBroadCastSocket = new Thread(
                 () -> {
