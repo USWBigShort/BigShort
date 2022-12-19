d56d05b5744a542af8a764ba5c78a7e2c15adddc
Abs0c <qjfjrdl12@naver.com>
2022-12-19 14:26:30 +0900

    1. makeUserThread에서 User를 생성하도록 했음 2. Main에서 유저를 생성하는 스레드인 makeUserThread를 실행하도록 코드를 추가했음 3. User 클래스의 toString()을 유저에 대한 간단한 정보를 표기하도록 Override 했음 4. UserThread 클래스에서 "매수/매도 코인명 코인개수"의 형식으로 값을 입력받을때, 입력 파라미터 수가 모자라거나, 형태가 잘못된 경우를 try-catch 문으로 잡아서 해결했음

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,7 +1,10 @@
 import coin.CoinController;
+import user.makeUserThread;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -16,38 +19,42 @@ public class Main {
         cmg.makeCoin("이더", 5000, 500, 1);
         cmg.makeCoin("비트", 3000, 2000, 2);
         cmg.makeCoin("샘", 4000, 60, 3);*/
-        while (true) {
-            System.out.println("코인을 생성하려면 make를 입력해주세요");
-            String str = sc.nextLine();
-            if (str.equals("make")) {
-                String name; int amount, price; double rateOfChange;
+        try(ServerSocket serverSocket = new ServerSocket(8888)){
+            new Thread(new makeUserThread(serverSocket)).start();
+            while (true) {
+                System.out.println("코인을 생성하려면 make를 입력해주세요");
+                String str = sc.nextLine();
+                if (str.equals("make")) {
+                    String name; int amount, price; double rateOfChange;
 
-                System.out.print("생성하려는 코인의 이름, 수량, 가격, 변동률을 입력해주세요: ");
-                StringTokenizer input = new StringTokenizer(sc.nextLine());
+                    System.out.print("생성하려는 코인의 이름, 수량, 가격, 변동률을 입력해주세요: ");
+                    StringTokenizer input = new StringTokenizer(sc.nextLine());
 
-                name = input.nextToken();
-                amount = Integer.parseInt(input.nextToken());
-                price = Integer.parseInt(input.nextToken());
-                rateOfChange = Double.parseDouble(input.nextToken());
+                    name = input.nextToken();
+                    amount = Integer.parseInt(input.nextToken());
+                    price = Integer.parseInt(input.nextToken());
+                    rateOfChange = Double.parseDouble(input.nextToken());
 
-                cmg.makeCoin(name, amount, price, rateOfChange);
-                System.out.println("코인이 생성되었습니다.");
-                System.out.println();
+                    cmg.makeCoin(name, amount, price, rateOfChange);
+                    System.out.println("코인이 생성되었습니다.");
+                    System.out.println();
 
-            } else if (str.equals("exit")) {
-                break;
+                }else if (str.equals("exit")) {
+                    break;
+                }
             }
+            System.out.println("전체 출력");
+            cmg.printAllCoin();
+            System.out.println();
+
+            System.out.println("루나만 출력");
+            cmg.printCoin("루나");
+
+            System.out.println("도지 삭제");
+            cmg.removeCoin("도지");
+            cmg.printAllCoin();
+        } catch (IOException e){
+            e.printStackTrace();
         }
-        System.out.println("전체 출력");
-        cmg.printAllCoin();
-        System.out.println();
-
-        System.out.println("루나만 출력");
-        cmg.printCoin("루나");
-
-        System.out.println("도지 삭제");
-        cmg.removeCoin("도지");
-        cmg.printAllCoin();
-
     }
 }
