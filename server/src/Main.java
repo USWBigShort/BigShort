755e6cb07d0a3ba1d5e80a1d409343ad7e8b019d
minseob <minseob15@naver.com>
2022-12-19 14:32:44 +0900

    서버 메세지 컨트롤러 생성

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,4 +1,5 @@
 import coin.CoinController;
+import message.ServerMessageController;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,48 +8,12 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        CoinController cmg = new CoinController();
-        Scanner sc = new Scanner(System.in);
+        CoinController coinController = new CoinController();
+        ServerMessageController serverMessageController = new ServerMessageController(coinController);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        /*cmg.makeCoin("루나", 7000, 3, 5);
-        cmg.makeCoin("도지", 6000, 20, 8);
-        cmg.makeCoin("이더", 5000, 500, 1);
-        cmg.makeCoin("비트", 3000, 2000, 2);
-        cmg.makeCoin("샘", 4000, 60, 3);*/
-        while (true) {
-            System.out.println("코인을 생성하려면 make를 입력해주세요");
-            String str = sc.nextLine();
-            if (str.equals("make")) {
-                String name; int amount, price; double rateOfChange;
+        serverMessageController.run();
 
-                System.out.print("생성하려는 코인의 이름, 수량, 가격, 변동률을 입력해주세요: ");
-                StringTokenizer input = new StringTokenizer(sc.nextLine());
-
-                name = input.nextToken();
-                amount = Integer.parseInt(input.nextToken());
-                price = Integer.parseInt(input.nextToken());
-                rateOfChange = Double.parseDouble(input.nextToken());
-
-                cmg.makeCoin(name, amount, price, rateOfChange);
-                System.out.println("코인이 생성되었습니다.");
-                System.out.println();
-
-            } else if (str.equals("exit")) {
-                break;
-            }
-        }
-
-        System.out.println("전체 출력");
-        cmg.printAllCoin();
-
-        cmg.changeCoinRateByRandom("루나");
-        cmg.printAllCoin();
-
-        cmg.changeCoinRateByRandom("도지");
-        cmg.printAllCoin();
-
-        cmg.changeCoinRateByRandom("이더");
 
     }
 }
