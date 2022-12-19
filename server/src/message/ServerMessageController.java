951ce2328e0b9442699fa5c7617bda7a54fec07d
minseob <minseob15@naver.com>
2022-12-19 15:18:40 +0900

    서버 메세지 컨트롤러 switch문으로 수정 Main에서 코드 간략화

--- server/src/message/ServerMessageController.java
+++ server/src/message/ServerMessageController.java
@@ -20,93 +20,98 @@ public class ServerMessageController implements Runnable {
         System.out.println("서버 메세지 컨트롤러 생성");
         System.out.println("메세지 입력에 대해 궁금하시면 리스트를 입력해주세요.");
 
+        label:
         while (true) {
             String str = sc.nextLine();
 
-            if (str.equals("코인생성")) {
-                String coinName;
-                int coinAmount, coinPrice;
-                double coinRateOfChange;
+            switch (str) {
+                case "코인생성": {
+                    String coinName;
+                    int coinAmount, coinPrice;
+                    double coinRateOfChange;
 
-                System.out.print("생성하려는 코인의 이름, 수량, 가격, 변동률을 입력해주세요: ");
-                input = new StringTokenizer(sc.nextLine());
+                    System.out.print("생성하려는 코인의 이름, 수량, 가격, 변동률을 입력해주세요: ");
+                    input = new StringTokenizer(sc.nextLine());
 
-                coinName = input.nextToken();
-                coinAmount = Integer.parseInt(input.nextToken());
-                coinPrice = Integer.parseInt(input.nextToken());
-                coinRateOfChange = Double.parseDouble(input.nextToken());
+                    coinName = input.nextToken();
+                    coinAmount = Integer.parseInt(input.nextToken());
+                    coinPrice = Integer.parseInt(input.nextToken());
+                    coinRateOfChange = Double.parseDouble(input.nextToken());
 
-                coinController.makeCoin(coinName, coinAmount, coinPrice, coinRateOfChange);
-                System.out.println(coinName + "코인이 생성되었습니다.");
-                System.out.println();
+                    coinController.makeCoin(coinName, coinAmount, coinPrice, coinRateOfChange);
+                    System.out.println(coinName + "코인이 생성되었습니다.");
+                    System.out.println();
 
-            }
-            else if (str.equals("코인삭제")) {
-                String name;
-                System.out.print("삭제하려는 코인의 이름을 입력해주세요: ");
-                input = new StringTokenizer(sc.nextLine());
+                    break;
+                }
+                case "코인삭제":
+                    String name;
+                    System.out.print("삭제하려는 코인의 이름을 입력해주세요: ");
+                    input = new StringTokenizer(sc.nextLine());
 
-                name = input.nextToken();
+                    name = input.nextToken();
 
-                coinController.removeCoin(name);
-                System.out.println(name + "코인이 삭제되었습니다.");
-                System.out.println();
-            }
-            else if (str.equals("코인 리스트")) {
-                System.out.println("전체 코인리스트입니다.");
-                coinController.printAllCoin();
-            }
-            else if (str.equals("랜덤변동률")) {
-                String coinName;
-                int changeRateRange;
+                    coinController.removeCoin(name);
+                    System.out.println(name + "코인이 삭제되었습니다.");
+                    System.out.println();
+                    break;
+                case "코인 리스트":
+                    System.out.println("전체 코인리스트입니다.");
+                    coinController.printAllCoin();
+                    break;
+                case "랜덤변동률": {
+                    String coinName;
+                    int changeRateRange;
 
-                System.out.print("변동시킬 코인 이름과 변동률의 범위를 입력해주세요: ");
-                input = new StringTokenizer(sc.nextLine());
+                    System.out.print("변동시킬 코인 이름과 변동률의 범위를 입력해주세요: ");
+                    input = new StringTokenizer(sc.nextLine());
 
-                coinName = input.nextToken();
-                changeRateRange = Integer.parseInt(input.nextToken());
+                    coinName = input.nextToken();
+                    changeRateRange = Integer.parseInt(input.nextToken());
 
-                coinController.changeCoinRateByRandom(coinName, changeRateRange);
-                System.out.println(coinName+ " 코인의 변동률이 변동되었습니다.");
-                System.out.println();
-            }
-            else if (str.equals("랜덤가격")) {
-                String coinName;
-                int changeRateRange;
+                    coinController.changeCoinRateByRandom(coinName, changeRateRange);
+                    System.out.println(coinName + " 코인의 변동률이 변동되었습니다.");
+                    System.out.println();
+                    break;
+                }
+                case "랜덤가격": {
+                    String coinName;
+                    int changeRateRange;
 
-                System.out.print("변동시킬 코인 이름과 가격의 범위를 입력해주세요: ");
-                input = new StringTokenizer(sc.nextLine());
+                    System.out.print("변동시킬 코인 이름과 가격의 범위를 입력해주세요: ");
+                    input = new StringTokenizer(sc.nextLine());
 
-                coinName = input.nextToken();
-                changeRateRange = Integer.parseInt(input.nextToken());
+                    coinName = input.nextToken();
+                    changeRateRange = Integer.parseInt(input.nextToken());
 
-                coinController.changeCoinPriceByRandom(coinName, changeRateRange);
-                System.out.println(coinName+ " 코인의 가격이 변동되었습니다.");
-                System.out.println();
-            }
-            else if (str.equals("랜덤수량")) {
-                String coinName;
-                int changeRateRange;
+                    coinController.changeCoinPriceByRandom(coinName, changeRateRange);
+                    System.out.println(coinName + " 코인의 가격이 변동되었습니다.");
+                    System.out.println();
+                    break;
+                }
+                case "랜덤수량": {
+                    String coinName;
+                    int changeRateRange;
 
-                System.out.print("변동시킬 코인 이름과 수량의 범위를 입력해주세요: ");
-                input = new StringTokenizer(sc.nextLine());
+                    System.out.print("변동시킬 코인 이름과 수량의 범위를 입력해주세요: ");
+                    input = new StringTokenizer(sc.nextLine());
 
-                coinName = input.nextToken();
-                changeRateRange = Integer.parseInt(input.nextToken());
+                    coinName = input.nextToken();
+                    changeRateRange = Integer.parseInt(input.nextToken());
 
-                coinController.changeCoinAmountByRandom(coinName, changeRateRange);
-                System.out.println(coinName+ " 코인의 수량이 변동되었습니다.");
-                System.out.println();
-            }
-
-            else if (str.equals("리스트")) {
-                System.out.println("서버 메세지로는 \"코인생성, 코인삭제, 코인 리스트, 랜덤변동률, 랜덤가격, 랜덤수량, 리스트, 나가기\"가 있습니다. ");
-            }
-            else if (str.equals("나가기")) {
-                break;
-            }
-            else {
-                System.out.println("잘못된 메세지입니다. 리스트를 참조해주십시오!");
+                    coinController.changeCoinAmountByRandom(coinName, changeRateRange);
+                    System.out.println(coinName + " 코인의 수량이 변동되었습니다.");
+                    System.out.println();
+                    break;
+                }
+                case "리스트":
+                    System.out.println("서버 메세지로는 \"코인생성, 코인삭제, 코인 리스트, 랜덤변동률, 랜덤가격, 랜덤수량, 리스트, 나가기\"가 있습니다. ");
+                    break;
+                case "나가기":
+                    break label;
+                default:
+                    System.out.println("잘못된 메세지입니다. 리스트를 참조해주십시오!");
+                    break;
             }
         }
     }
